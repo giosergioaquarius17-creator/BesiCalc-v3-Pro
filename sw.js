ae61db158f6dfbd9f7eab6b433f0875e883ad067
giosergioaquarius17-creator <giosergioaquarius17@gmail.com>
2026-02-27 21:19:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
-const cacheName = 'besicalc-v3';
-const assets = ['./', './index.html', './manifest.json'];
+const cacheName = 'besicalc-pro-v4';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2641/2641434.png'
+];
 
+// Tahap Install: Menyimpan file inti ke dalam cache
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Tahap Aktivasi: Membersihkan cache lama jika ada update versi
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Tahap Fetch: Mengambil data dari cache jika offline
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(cacheRes => {
+      return cacheRes || fetch(e.request);
+    })
+  );
 });
